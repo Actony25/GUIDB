81f5ad18cbd3b0315526c63d1ad1311a3bc0d308
Anthony Cangelosi <actony25@hotmail.com>
2018-11-17 13:02:46 -0500

    Initial commit with comments

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -21,14 +21,18 @@ public class Controller {
   void connect(ActionEvent event) {
 
     final String DATABASE_URL = "jdbc:derby:lib//books";
+    //Creating the SQL statement
     final String SELECT_QUERY =
         "SELECT authorID, firstName, lastName FROM authors";
 
     // use try-with-resources to connect to and query the database
     try (
+        //Connecting to the derby database
         Connection connection = DriverManager.getConnection(
+            //Entering the predefined username and password
             DATABASE_URL, "deitel", "deitel");
         Statement statement = connection.createStatement();
+        //Executing the query variable
         ResultSet resultSet = statement.executeQuery(SELECT_QUERY)) {
       resultSet.next();
       lblFirst.setText(resultSet.getString(2));
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Setting the stage
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Setting the title
         primaryStage.setTitle("Hello World");
+        //Setting the width and height
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
